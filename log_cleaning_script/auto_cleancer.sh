80c345528fdb09182549d73bc77961950f7d9dc6
Adelfo <adelfodangf@gmail.com>
2025-05-13 16:48:58 -0600

    added the script for auto cleaning all instances logs

--- log_cleaning_script/auto_cleancer.sh
+++ log_cleaning_script/auto_cleancer.sh
@@ -5,8 +5,8 @@ IP2="54.226.44.126"
 CLAVE_PEM="~/.ssh/clave.pem"
 
 # Ejecutar el script de limpieza en ambas instancias en paralelo
-ssh -i $CLAVE_PEM ec2-user@$IP1 'bash -s' < /ruta/al/script/limpiar_logs.sh &
-ssh -i $CLAVE_PEM ec2-user@$IP2 'bash -s' < /ruta/al/script/limpiar_logs.sh &
+ssh -i $CLAVE_PEM ec2-user@$IP1 'bash -s' < limpiar_logs.sh &
+ssh -i $CLAVE_PEM ec2-user@$IP2 'bash -s' < limpiar_logs.sh &
 wait
 
 echo "Limpieza de logs realizada en ambas instancias."
